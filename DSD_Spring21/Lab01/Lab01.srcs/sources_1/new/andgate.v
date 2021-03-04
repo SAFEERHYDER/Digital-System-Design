92c0236eb495f7679077937218e3aa86949c9b6c
Safeer Hyder <safeer.hyder@iba-suk.edu.pk>
2021-03-04 18:51:58 +0500

    Create andgate.v

---
+++ DSD_Spring21/Lab01/Lab01.srcs/sources_1/new/andgate.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/21/2020 04:38:55 PM
+// Design Name: 
+// Module Name: andgate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+// start of module 
+module andgate(Out, In1, In2);
+
+// Port dimensions
+input In1, In2;
+output Out;
+
+
+// Logic
+and and1(Out, In1, In2);
+
+// end of module 
+endmodule
+
+
+
