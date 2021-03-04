85ee6f759c29f1d1041f3dfc05bdfd51bbf7dbb8
Safeer Hyder <safeer.hyder@iba-suk.edu.pk>
2021-03-04 18:11:52 +0500

    Create Multiplexer_4_to_1.v

---
+++ DSD_Spring21/Lab01/Lab01.srcs/sim_1/new/Multiplexer_4_to_1.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/04/2021 06:11:04 PM
+// Design Name: 
+// Module Name: Multiplexer_4_to_1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Multiplexer_4_to_1(
+
+    );
+endmodule
