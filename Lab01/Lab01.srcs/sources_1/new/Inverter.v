dafd32f5fb4a6d19c66518bb81f75c793a0f299f
Safeer Hyder <safeer.hyder@iba-suk.edu.pk>
2021-03-05 09:59:48 +0500

    Create Inverter.v

---
+++ Lab01/Lab01.srcs/sources_1/new/Inverter.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/25/2021 02:21:05 PM
+// Design Name: 
+// Module Name: Inverter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: Safeer Hyder 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Inverter(A, A_not);
+
+// ports dimensions, size
+input  A; 
+output A_not; 
+
+// logic
+not not1(A_not, A);
+
+endmodule
