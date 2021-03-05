09963a88544d4032cccf2bf06f6919cf1328c45a
Safeer Hyder <safeer.hyder@iba-suk.edu.pk>
2021-03-05 16:41:15 +0500

    Update Sixteen_Bit_Adder.v

--- Lab01/Lab01.srcs/sources_1/new/Sixteen_Bit_Adder.v
+++ Lab01/Lab01.srcs/sources_1/new/Sixteen_Bit_Adder.v
@@ -27,10 +27,10 @@ output [15:0] Sum;
 output Carry;
 
 // logic
-Four_bit_Adder Fourbit1(.Sum(Sum[3:0]), .Carry(c1), .A(A[3:0]), .B(B[3:0]), .Cin(Cin));
-Four_bit_Adder Fourbit2(.Sum(Sum[7:4]), .Carry(c2), .A(A[7:4]), .B(B[7:4]), .Cin(c1));
-Four_bit_Adder Fourbit3(.Sum(Sum[11:8]), .Carry(c3), .A(A[11:8]), .B(B[11:8]), .Cin(c2));
-Four_bit_Adder Fourbit4(.Sum(Sum[15:12]), .Carry(Carry), .A(A[15:12]), .B(B[15:12]), .Cin(c3));
+Parallel_Adder Fourbit1(.Sum(Sum[3:0]), .Carry(c1), .A(A[3:0]), .B(B[3:0]), .Cin(Cin));
+Parallel_Adder Fourbit2(.Sum(Sum[7:4]), .Carry(c2), .A(A[7:4]), .B(B[7:4]), .Cin(c1));
+Parallel_Adder Fourbit3(.Sum(Sum[11:8]), .Carry(c3), .A(A[11:8]), .B(B[11:8]), .Cin(c2));
+Parallel_Adder Fourbit4(.Sum(Sum[15:12]), .Carry(Carry), .A(A[15:12]), .B(B[15:12]), .Cin(c3));
 
 
 
