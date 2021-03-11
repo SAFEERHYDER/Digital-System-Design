37ed2e07a8062606fbb6c6d66388aed812e0ccf0
Safeer Hyder <54274466+SAFEERHYDER@users.noreply.github.com>
2021-03-11 14:27:57 +0500

    Add files via upload

---
+++ Lab03/Lab03.srcs/sources_1/new/Four_to_One_Mux.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/11/2021 01:20:54 PM
+// Design Name: 
+// Module Name: Four_to_One_Mux
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Four_to_One_Mux(out, s0, s1, d0, d1, d2, d3);
+input wire s0, s1, d0, d1, d2, d3;
+output reg out;
+
+// logic desciption
+// using behavorial 
+always@(s0, s1, d0, d1, d2, d3)
+begin
+    if (s1 == 1'b0  && s0 == 1'b0)
+            out = d0;
+    else if (s1 == 1'b0 && s0 == 1'b1)
+            out = d1;
+    else if (s1 == 1'b1 && s0 == 1'b0)
+            out = d2;
+    else if (s1 == 1'b1 && s0 == 1'b1)
+            out = d3;
+    else
+            out = 1'bz;     
+end 
+
+ 
+
+endmodule
